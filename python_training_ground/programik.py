b8e1249aa663e9dd0d1ec4f991f6fdfe28eb9220
DubanZe <dubanze@DubanZes-MacBook-Pro.local>
2021-08-19 22:48:23 +0200

    further exercises

--- python_training_ground/programik.py
+++ python_training_ground/programik.py
@@ -193,54 +193,76 @@
 
 # input("\nAby zakończyć program, naciśnij klawisz Enter.")
 
-import random
-from typing import Counter
+##### GRA W ZGADYWANIE SLOWA NA PODSTAWIE ILOSCI LITER
+# import random
+# from typing import Counter
 
-WORDS = ("samochód",
-         "krzesło",
-         "masakra",
-         "skomplikowany",
-         "wiertarka",
-         "materac")
+# WORDS = ("samochód",
+#          "krzesło",
+#          "masakra",
+#          "skomplikowany",
+#          "wiertarka",
+#          "materac")
 
-word = random.choice(WORDS)
-correct = word
+# word = random.choice(WORDS)
+# correct = word
 
-print("Słowo, które masz odgadnąć, zawiera " + str(len(correct)) + " liter.")
+# print("Słowo, które masz odgadnąć, zawiera " + str(len(correct)) + " liter.")
 
 
-print("Mozesz pięciokrotnie podać jakąś literę, a ja powiem Ci ile razy \
-    znajduje się ona w tym słowie. Potem będziesz musiał je zgadnąć.")
+# print("Mozesz pięciokrotnie podać jakąś literę, a ja powiem Ci ile razy \
+#     znajduje się ona w tym słowie. Potem będziesz musiał je zgadnąć.")
 
-letter = input("Podaj literę: ").lower()
+# letter = input("Podaj literę: ").lower()
 
-if word.count(letter) > 0:
-    print("Tak, litera " + letter + " znajduje się w tym słowie.")
-else:
-    print("Nie, litera " + letter + " nie znajduje się w tym słowie.")
+# if word.count(letter) > 0:
+#     print("Tak, litera " + letter + " znajduje się w tym słowie.")
+# else:
+#     print("Nie, litera " + letter + " nie znajduje się w tym słowie.")
 
-# if letter != "":
-#     print("Litera " + letter + " występuje w tym słowie " + str(word.count(letter)) + " razy.")
+# # if letter != "":
+# #     print("Litera " + letter + " występuje w tym słowie " + str(word.count(letter)) + " razy.")
 
-tries = 1
+# tries = 1
 
-while letter != "" and tries < 5:    
-    letter = input("Mozesz podać następną: ").lower()
-    if word.count(letter) > 0:
-        print("Tak, litera " + letter + " znajduje się w tym słowie.")
-    else:
-        print("Nie, litera " + letter + " nie znajduje się w tym słowie.")
-    # print("Litera " + letter + " występuje w tym słowie " + str(word.count(letter)) + " razy.")
-    tries += 1
+# while letter != "" and tries < 5:    
+#     letter = input("Mozesz podać następną: ").lower()
+#     if word.count(letter) > 0:
+#         print("Tak, litera " + letter + " znajduje się w tym słowie.")
+#     else:
+#         print("Nie, litera " + letter + " nie znajduje się w tym słowie.")
+#     # print("Litera " + letter + " występuje w tym słowie " + str(word.count(letter)) + " razy.")
+#     tries += 1
 
-if letter == "":
-    print("Szkoda, ze nie podałeś zadnej litery.")
+# if letter == "":
+#     print("Szkoda, ze nie podałeś zadnej litery.")
 
-if tries == 5:
-    usrword = input("Odgadnij teraz proszę jakie to słowo?: ")
-    if usrword.lower() == correct:
-        print("Brawo! Zgadłeś.")
-    elif usrword.lower() != correct:
-        print("Niestety, spróbuj ponownie!")
+# if tries == 5:
+#     usrword = input("Odgadnij teraz proszę jakie to słowo?: ")
+#     if usrword.lower() == correct:
+#         print("Brawo! Zgadłeś.")
+#     elif usrword.lower() != correct:
+#         print("Niestety, spróbuj ponownie!")
 
-input("Naciśnij Enter, aby zakończyć.")
+# input("Naciśnij Enter, aby zakończyć.")
+
+##### PROGRAM WYPISUJACY LOSOWO SLOWA, A NASTEPNIE USUWAJACY JE
+# import random
+
+# WORDS = ["samochód",
+#          "krzesło",
+#          "masakra",
+#          "skomplikowany",
+#          "wiertarka",
+#          "materac"]
+
+# length = len(WORDS)
+
+# while length > 0:
+#     index = random.randrange(length)
+#     print(WORDS[index])
+#     del WORDS[index]
+#     length -= 1
+
+# input("Naciśnij Enter, aby zakończyć.")
+
